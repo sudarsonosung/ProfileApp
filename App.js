816763d72cfc91bda3273356a199199d12e5e4f1
sudarsonosung <sudarsono.sung@gmail.com>
2018-11-29 22:26:13 +0700

    first commit project ProfileApp

---
+++ App.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet
+} from "react-native";
+import InputFormAddress from './src/inputs/InputFormAddress'
+import InputFormGender from './src/inputs/InputFormGender'
+import InputFormName from './src/inputs/InputFormName'
+import InputFormPhone from './src/inputs/InputFormPhone'
+import Main from './src/Main'
+import Result from './src/Result'
+
+class App extends Component {
+  render() {
+    return <Main />
+  }
+}
+
+export default App;
+
+// 1. buat 4-pairs inputan
+//2.
